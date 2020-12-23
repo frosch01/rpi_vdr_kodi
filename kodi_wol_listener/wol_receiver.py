a106f1998af3c86ff3723c79991f4db68e0ef591
Ralph Scharpf <ralph.scharpf@gmail.com>
2020-12-23 13:28:09 +0100

    ADDED first test

--- kodi_wol_listener/wol_receiver.py
+++ kodi_wol_listener/wol_receiver.py
@@ -20,15 +20,25 @@ class WolReceiver(asyncio.DatagramProtocol):
 
     def datagram_received(self, data, addr):
         super().datagram_received(data, addr)
-        if b'\xff'*6 == data[:6] and self.mymac_bytes == data[6:12]:
+        accepted = b'\xff'*6 + self.mymac_bytes == data[:12]
+        if accepted:
             self.wol_callback(addr)
+        logging.debug("WOL listener %s from %s:%d: %s",
+                      'accepted' if accepted else 'rejected',
+                      addr[0], addr[1],
+                      data.hex())
 
-    async def init(self, port):
+    async def init(self, port=0):
         """Async initialization method
 
         Args:
             port (int) UDP port the receiver shall be listening
+        Returns:
+            (int) The port actually taken (in case of 0 port argument)
         """
-        logging.debug("WOL listener running")
         loop = asyncio.get_running_loop()
         await loop.create_datagram_endpoint(lambda: self, local_addr=('0.0.0.0', port))
+        sock = self.transport.get_extra_info('socket')
+        port = sock.getsockname()[1]
+        logging.debug("WOL listener running on port %d", port)
+        return port
--- tests/test_wol_receiver.py
+++ tests/test_wol_receiver.py
@@ -1,4 +1,36 @@
 import pytest
 
-def test_init():
+# Code has low complexity. A single function is enough
+@pytest.mark.asyncio
+async def test_all(caplog, mocker, event_loop):
     from kodi_wol_listener.wol_receiver import WolReceiver
+    import socket
+    import getmac
+    import asyncio
+
+    caplog.set_level('DEBUG')
+    # create a mock callback that triggers termintate future
+    terminate = event_loop.create_future()
+    callback = mocker.Mock()
+    def callback_func(*args):
+        terminate.set_result(None)
+    callback.side_effect = callback_func
+    # Initialize WolReceiver
+    wol = WolReceiver(callback)
+    dst_port = await wol.init()
+    # Create socket for sending wakeup pattern
+    sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    sock.bind(('127.0.0.1', 0))
+    src_addr = sock.getsockname()
+    dst_addr = ('127.0.0.1', dst_port)
+
+    # Send an arbitrary packet -> Rejected
+    sock.sendto(b'hello world', dst_addr)
+    # Send a valid WOL packet -> Accepted
+    mac_hex_str = getmac.get_mac_address(interface="eth0")
+    mac_bytes = bytes.fromhex(mac_hex_str.replace(':', ''))
+    sock.sendto(b'\xff' * 6 + mac_bytes, dst_addr)
+
+    # Evaluate results, a single call is expected
+    await asyncio.wait_for(terminate, 1)
+    callback.assert_called_once_with(src_addr)
