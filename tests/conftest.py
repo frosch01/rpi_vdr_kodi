b28fb3099e70aebdef2d6c683712fb863628fc99
Ralph Scharpf <ralph.scharpf@gmail.com>
2020-12-24 10:02:07 +0100

    ADDED test cases for hdmi interface

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,3 +3,20 @@ import os
 import pytest
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+@pytest.fixture
+def mock_coroutine(mocker, monkeypatch):
+    def _create_coro_patch(*args):
+        """Fixture to mock a corotine. If an object path is given, monkeypatch
+        the coroutine with the mocked one.
+
+        Args:
+            args (list[str]) Arguments for monkeypatch, e.g. the object path
+        """
+        coro_mock = mocker.Mock()
+        async def _coroutine(*args, **kwargs):
+            return coro_mock(*args, **kwargs)
+        if args:
+            monkeypatch.setattr(*args, _coroutine)
+        return _coroutine, coro_mock
+    return _create_coro_patch
