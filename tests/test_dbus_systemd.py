9df51e031a44c1e8497b02f4089ba7a29ca13c3e
Ralph Scharpf <ralph.scharpf@gmail.com>
2021-01-04 12:08:01 +0100

    ADDED systemd manager test, made systemd tests to be dependent on dbus/systemd

--- tests/test_dbus_systemd.py
+++ tests/test_dbus_systemd.py
@@ -1,7 +1,30 @@
+import os
 import pytest
 
-@pytest.mark.asyncio
-async def test_dbus_systemd():
+pytestmark = [
+    pytest.mark.skipif(not os.environ.get('XDG_RUNTIME_DIR'), reason='Systemd environment required'),
+    pytest.mark.skipif(not os.environ.get('DBUS_SESSION_BUS_ADDRESS'), reason='DBUS environment required')]
+
+@pytest.fixture
+async def systemd():
     from kodi_wol_listener.dbus_systemd import DbusSystemd
-    bus = await DbusSystemd().init()
-    await bus.get_object('/org/freedesktop/systemd1')
+    return await DbusSystemd().init()
+
+
+@pytest.mark.asyncio
+async def test_dbus_systemd(systemd):
+    await systemd.get_object('/org/freedesktop/systemd1')
+
+
+@pytest.mark.asyncio
+async def test_manager(systemd):
+    import os
+    from kodi_wol_listener.dbus_systemd import SystemdManager
+    manager = await SystemdManager().init(systemd)
+    await manager.link_unit(os.path.dirname(__file__) + '/dummy.service')
+    await manager.reload()
+    await manager.enable_unit('dummy.service')
+    await manager.start_unit('dummy.service')
+    await manager.stop_unit('dummy.service')
+    await manager.disable_unit('dummy.service')
+    await manager.reload()
